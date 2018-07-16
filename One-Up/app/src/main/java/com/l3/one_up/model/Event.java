ac4bff82fdbc95b9ebc4c7361c1a0e0027341c84
lucieleblanc <lucieleblanc425@gmail.com>
2018-07-16 13:38:10 -0700

    create and register Event model

---
+++ One-Up/app/src/main/java/com/l3/one_up/model/Event.java
@@ -0,0 +1,46 @@
+package com.l3.one_up.model;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import org.json.JSONObject;
+
+public class Event extends ParseObject {
+
+    private static final String KEY_USER = "user";
+    private static final String KEY_ACTIVITY = "activity";
+    private static final String KEY_INPUT = "inputType";
+    private static final String KEY_TOTALXP = "totalXP";
+
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user) {
+        put(KEY_USER, user);
+    }
+
+    public ParseObject getActivity() {
+        return getParseObject(KEY_ACTIVITY);
+    }
+
+    public void setActivity(ParseObject activity) {
+        put(KEY_ACTIVITY, activity);
+    }
+
+    public JSONObject getInputType() {
+        return getJSONObject(KEY_INPUT);
+    }
+
+    public void setInputType(JSONObject inputType) {
+        put(KEY_INPUT, inputType);
+    }
+
+    public int getTotalXP() {
+        return getInt(KEY_TOTALXP);
+    }
+
+    public void setTotalXP(int points) {
+        put(KEY_TOTALXP, points);
+    }
+}
