c33f2a57c6bdc2c05c41f6bed8bd49f11520db8f
LuzCappuccino <luz.camacho323@gmail.com>
2018-07-17 14:58:23 -0700

    Removed all test code. Ready for master branch merge

--- One-Up/app/src/main/java/com/l3/one_up/MainActivity.java
+++ One-Up/app/src/main/java/com/l3/one_up/MainActivity.java
@@ -13,22 +13,9 @@ import android.support.v4.app.FragmentTransaction;
 import com.l3.one_up.R;
 
 public class MainActivity extends AppCompatActivity {
-    public Button testButton;
-    public FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        testButton = findViewById(R.id.btTest);
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fragmentManager = getSupportFragmentManager();
-                Fragment fragmentActivity = new ActivitySelectionFragment();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.BigBoi, fragmentActivity).commit();
-            }
-        });
     }
 }
