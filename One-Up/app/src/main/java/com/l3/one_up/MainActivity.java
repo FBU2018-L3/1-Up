072ffd252bde3e8fa02b5bd189f71df4d9289b4a
lucieleblanc <lucieleblanc425@gmail.com>
2018-07-18 14:14:24 -0700

    clicking profile picture in home opens new activity with bottom nav bar

--- One-Up/app/src/main/java/com/l3/one_up/MainActivity.java
+++ One-Up/app/src/main/java/com/l3/one_up/MainActivity.java
@@ -1,12 +1,11 @@
 package com.l3.one_up;
 
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
-
-import com.l3.one_up.R;
 
 public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener {
 
@@ -15,19 +14,30 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        HomeFragment fragmentDemo = HomeFragment.newInstance();
-        ft.replace(R.id.fragmentHolder, fragmentDemo);
-        ft.commit();
+        HomeFragment homeFragment = HomeFragment.newInstance();
+        startFragment(homeFragment);
     }
 
     @Override
     public void onCategoryClick(String categoryName) {
         Toast.makeText(getApplicationContext(), categoryName + " button was clicked!", Toast.LENGTH_SHORT).show();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ActivitySelectionFragment fragmentDemo = ActivitySelectionFragment.newInstance(categoryName);
-        ft.replace(R.id.fragmentHolder, fragmentDemo);
-        ft.addToBackStack("tag").commit();
+        ActivitySelectionFragment activitySelectionFragment = ActivitySelectionFragment.newInstance(categoryName);
+        startFragment(activitySelectionFragment);
+    }
 
+    @Override
+    public void onProfilePictureClick() {
+        startActivity(ProfileActivity.class);
+    }
+
+    private void startActivity(Class activityClass) {
+        Intent i = new Intent(this, activityClass);
+        startActivity(i);
+    }
+
+    private void startFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragmentHolder, fragment);
+        ft.addToBackStack("main").commit();
     }
 }
---
+++ One-Up/app/src/main/java/com/l3/one_up/ProfileActivity.java
@@ -0,0 +1,50 @@
+package com.l3.one_up;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class ProfileActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_stats:
+                        // do something here
+                        return true;
+                    case R.id.action_profile:
+                        return true;
+                    case R.id.action_timeline:
+                        // do something here
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void startFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragmentHolder, fragment);
+        ft.addToBackStack("main").commit();
+    }
+
+    @Override
+    public void onLogoutClicked(Uri uri) {
+        // TODO - log out
+    }
+}
