df24335c89f98ca616bd1e797d4b8f4a231cf351
LuzCappuccino <luz.camacho323@gmail.com>
2018-07-19 12:18:16 -0700

    Finished adapter, needs testing

--- One-Up/app/src/main/java/com/l3/one_up/adapters/FeedItemAdapter.java
+++ One-Up/app/src/main/java/com/l3/one_up/adapters/FeedItemAdapter.java
@@ -1,17 +1,24 @@
 package com.l3.one_up.adapters;
 
 import android.content.Context;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.l3.one_up.R;
+import com.l3.one_up.model.Activity;
 import com.l3.one_up.model.Event;
+import com.parse.ParseObject;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by luzcamacho on 7/19/18.
@@ -42,24 +49,42 @@ public class FeedItemAdapter extends RecyclerView.Adapter<FeedItemAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull FeedItemAdapter.ViewHolder holder, int position) {
-
+        Event event = recentEvents.get(position);
+        /* parse out the activity, draw its name */
+        Activity activity = (Activity) event.getActivity();
+        holder.tvEventName.setText(activity.getName());
+        /* get the parse input object */
+        JSONObject inputTypes = event.getInputType();
+        Iterator iter = inputTypes.keys();
+        while (iter.hasNext()) {
+            Log.d(tag, iter.next().toString());
+        }
+        holder.tvEventNum.setText(":)");
+        holder.tvEventNumType.setText(":')");
+        /* set date and exp points gained */
+        holder.tvEventDate.setText(event.getDate("createdAt").toString());
+        holder.tvEventEXP.setText(String.valueOf(event.getTotalXP()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return recentEvents.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView tvEventName;
         public TextView tvEventEXP;
         public TextView tvEventDate;
+        public TextView tvEventNum;
+        public TextView tvEventNumType;
 
         public ViewHolder(View itemView) {
             super(itemView);
             tvEventName = itemView.findViewById(R.id.tvEventName);
             tvEventEXP = itemView.findViewById(R.id.tvEventEXP);
             tvEventDate = itemView.findViewById(R.id.tvEventDate);
+            tvEventNum = itemView.findViewById(R.id.tvEventNum);
+            tvEventNumType = itemView.findViewById(R.id.tvEventNumType);
         }
     }
 }
