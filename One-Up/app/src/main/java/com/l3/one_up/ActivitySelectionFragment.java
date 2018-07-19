e10f3b2263bc16b311f3763a9353134aef9aab8d
LuzCappuccino <luz.camacho323@gmail.com>
2018-07-19 10:36:09 -0700

    Finished search bar, changed colors

--- One-Up/app/src/main/java/com/l3/one_up/ActivitySelectionFragment.java
+++ One-Up/app/src/main/java/com/l3/one_up/ActivitySelectionFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,6 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.l3.one_up.fragments.InputFragment;
 import com.l3.one_up.model.Activity;
@@ -40,6 +42,8 @@ public class ActivitySelectionFragment extends Fragment implements ActivityItemA
     private ActivityItemAdapter itemAdapter;
     /* our command string that tells us which activities to load */
     public String category;
+    /* complete copy of all activities per category */
+    public ArrayList<Activity> completeActivities;
 
     private static String KEY_CATEGORY = "keyCategory";
 
@@ -70,6 +74,7 @@ public class ActivitySelectionFragment extends Fragment implements ActivityItemA
         rvActivityView = fragAct.findViewById(R.id.rvActivityView);
         /* init data set */
         myActivities = new ArrayList<>();
+        completeActivities = new ArrayList<>();
         /* init adapter */
         itemAdapter = new ActivityItemAdapter(myActivities, this);
         rvActivityView.setLayoutManager(new LinearLayoutManager(fragAct));
@@ -91,31 +96,52 @@ public class ActivitySelectionFragment extends Fragment implements ActivityItemA
         item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         SearchView searchView = new SearchView(getActivity());
 
-        /* modifying the text inside edittext component */
-        int id = searchView.getContext().getResources().getIdentifier("android:id/activitySearchBar", null, null);
+        /* Fetch our text view and set appropriate fields */
         TextView textView = (AutoCompleteTextView) searchView.findViewById(R.id.search_src_text);
-        Log.d(tag, "Id: " + id);
         textView.setHint("Search activities");
-        textView.setHintTextColor(getResources().getColor(R.color.testColor));
+        textView.setHintTextColor(getResources().getColor(R.color.hintColor));
         textView.setTextColor(getResources().getColor(R.color.writeColor));
-//
         /* implement the listener */
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 /* once the user hits the submit bar */
-                Log.d(tag, "submitted text");
+                doSearch(query);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
                 /* every time the user changes their search */
-                Log.d(tag, "changed text");
+                if(TextUtils.isEmpty(newText)){
+                    /* if the user has cleared the search, we want to show all activities */
+                    doSearch("");
+                }
                 return false;
             }
         });
         item.setActionView(searchView);
+    }
+
+
+    /* TODO: Check efficiency of this, look for ways to improve it or if it even matter (relatively small data set) */
+    /* actual search method for things */
+    private void doSearch(String query) {
+        if(query == ""){
+            /* empty query, nothing to search, show full activities */
+            myActivities.addAll(completeActivities);
+            itemAdapter.notifyDataSetChanged();
+            return;
+        }
+        myActivities.clear();
+        query.toLowerCase();
+        for(int i = 0; i < completeActivities.size(); i++){
+            String lowerName = completeActivities.get(i).getName().toLowerCase();
+            if(lowerName.contains(query)){
+                myActivities.add(completeActivities.get(i));
+            }
+        }
+        itemAdapter.notifyDataSetChanged();
     }
 
     private void loadActivities(String category) {
@@ -133,6 +159,7 @@ public class ActivitySelectionFragment extends Fragment implements ActivityItemA
                         Log.d(tag, "Name: " + myActivities.get(i).getName());
                         itemAdapter.notifyItemInserted(myActivities.size() - 1);
                     }
+                    completeActivities.addAll(myActivities);
                 }
                 else{
                     Log.d(tag, "Failed to load activies :(");
