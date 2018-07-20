0b4ebc6a059d2b905cfdde04a8e2b3c62696af75
lucieleblanc <lucieleblanc425@gmail.com>
2018-07-20 10:58:57 -0700

    create category adapter

---
+++ One-Up/app/src/main/java/com/l3/one_up/adapters/CategoryAdapter.java
@@ -0,0 +1,65 @@
+package com.l3.one_up.adapters;
+
+import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.l3.one_up.R;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+
+    private String[] categories;
+    private TypedArray categoryIcons;
+
+    public CategoryAdapter(String[] categories, TypedArray categoryIcons) {
+        this.categories = categories;
+        this.categoryIcons = categoryIcons;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View activityViewer = inflater.inflate(R.layout.item_category, parent, false);
+        ViewHolder viewHolder = new ViewHolder(activityViewer);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int fallbackImageId = android.R.drawable.ic_menu_crop;
+        holder.ivCategoryIcon.setImageResource(categoryIcons.getResourceId(position, fallbackImageId));
+        holder.tvCategoryName.setText(categories[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView tvCategoryName;
+        public ImageView ivCategoryIcon;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvCategoryName = itemView.findViewById(R.id.tvCategoryName);
+            ivCategoryIcon = itemView.findViewById(R.id.ivCategoryIcon);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION){
+                // callback -> homefragment
+            }
+        }
+    }
+}
--- One-Up/app/src/main/java/com/l3/one_up/fragments/HomeFragment.java
+++ One-Up/app/src/main/java/com/l3/one_up/fragments/HomeFragment.java
@@ -2,21 +2,22 @@ package com.l3.one_up.fragments;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
-import com.l3.one_up.listeners.OnUserTogglesSleepListener;
+
 import com.l3.one_up.R;
+import com.l3.one_up.adapters.CategoryAdapter;
+import com.l3.one_up.listeners.OnUserTogglesSleepListener;
 import com.parse.ParseUser;
 
 import butterknife.BindView;
@@ -32,8 +33,9 @@ public class HomeFragment extends Fragment {
     private TextView tvLevelNum;
     private TextView tvXPNum;
 
-    private ListView lvCategories;
-    private ArrayAdapter categoriesAdapter;
+    private RecyclerView rvCategories;
+    private CategoryAdapter categoriesAdapter;
+    private TypedArray categoryIcons;
     private String[] categories;
 
     private OnFragmentInteractionListener mListener;
@@ -99,17 +101,13 @@ public class HomeFragment extends Fragment {
         });
 
         categories = getResources().getStringArray(R.array.categories);
+        categoryIcons = getResources().obtainTypedArray(R.array.your_array_name);
 
-        categoriesAdapter = new ArrayAdapter<>(getContext(), R.layout.item_category, categories);
+        categoriesAdapter = new CategoryAdapter(categories, categoryIcons);
 
-        lvCategories = (ListView) getActivity().findViewById(R.id.lvCategories);
-        lvCategories.setAdapter(categoriesAdapter);
-        lvCategories.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                onButtonPressed(categories[position]);
-            }
-        });
+        rvCategories = (RecyclerView) getActivity().findViewById(R.id.rvCategories);
+        rvCategories.setAdapter(categoriesAdapter);
+
 
         tbSleepSwitch.setChecked(user.getBoolean("isAsleep"));
     }
