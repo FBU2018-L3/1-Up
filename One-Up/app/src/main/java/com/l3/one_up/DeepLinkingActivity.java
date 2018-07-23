b411a83d30f561b4929aaa9458e4c277b1d46e3f
LuzCappuccino <luz.camacho323@gmail.com>
2018-07-23 12:27:24 -0700

    Added some autho facebook stuff to gradles and manifest

---
+++ One-Up/app/src/main/java/com/l3/one_up/DeepLinkingActivity.java
@@ -0,0 +1,13 @@
+package com.l3.one_up;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeepLinkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_linking);
+    }
+}
