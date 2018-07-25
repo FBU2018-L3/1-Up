9bd0dc8ffae1c82923ff5ac6d37c899b9cb7e450
lucieleblanc <lucieleblanc425@gmail.com>
2018-07-25 14:42:00 -0700

    adapter for goal items in feed

--- One-Up/app/src/main/java/com/l3/one_up/adapters/GoalItemAdapter.java
+++ One-Up/app/src/main/java/com/l3/one_up/adapters/GoalItemAdapter.java
@@ -1,4 +1,56 @@
 package com.l3.one_up.adapters;
 
-public class GoalItemAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.l3.one_up.R;
+import com.l3.one_up.model.Goal;
+
+import java.util.ArrayList;
+
+public class GoalItemAdapter extends RecyclerView.Adapter<GoalItemAdapter.ViewHolder> {
+
+    private ArrayList<Goal> goals;
+
+    private Context context;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View activityViewer = inflater.inflate(R.layout.item_activity, parent, false);
+        ViewHolder viewHolder = new ViewHolder(activityViewer);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Goal myGoal = goals.get(position);
+        holder.tvGoalId.setText(myGoal.getObjectId());
+        holder.tvGoalName.setText(myGoal.getActivity().getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return goals.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvGoalId;
+        public TextView tvGoalName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvGoalId = (TextView) itemView.findViewById(R.id.tvGoalID);
+            tvGoalName = (TextView) itemView.findViewById(R.id.tvGoalName);
+        }
+    }
 }
