29d8dd1c455ef7152452f8d710359196affa816f
LuzCappuccino <luz.camacho323@gmail.com>
2018-07-27 09:58:58 -0700

    Tiny comments and fixes. it readey to go

--- One-Up/app/src/main/java/com/l3/one_up/fragments/FriendsFragment.java
+++ One-Up/app/src/main/java/com/l3/one_up/fragments/FriendsFragment.java
@@ -143,24 +143,25 @@ public class FriendsFragment extends Fragment implements FacebookCallComplete {
 
                     if(parseUsers.size() == FacebookList.size()){
                         setLevels(FacebookList, parseUsers);
+                        /* Note: might want to move all this stuff into the above conditional if I think it'll be a problem */
+                        updateFBAdapterDataSet(FacebookList);
+                        /* load in events for the first user in the data set */
+                        loadFriendEvents(parseUsers.get(0));
+                        rvFriendList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                            @Override
+                            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                                super.onScrollStateChanged(recyclerView, newState);
+                                if(newState == recyclerView.SCROLL_STATE_IDLE){
+                                    int position = ((GridLayoutManager) recyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition();
+                                    User atUser = parseUsers.get(position);
+                                    Log.d(tag, "At user: " + atUser.getFacebookId());
+                                    loadFriendEvents(atUser);
+                                }
+                            }
+                        });
                     } else {
                         Log.e(tag ,"UM NUMBER OF USERS DO NOT MATCH DO NOT TRY LINKING THE INFO TOGETHER");
                     }
-                    updateAdapterDataSet(FacebookList);
-                    loadFriendEvents(parseUsers.get(0));
-                    // TODO: launch this on the first element since ya know it doesn't actually do the thing till it scrolls
-                    rvFriendList.addOnScrollListener(new RecyclerView.OnScrollListener() {
-                        @Override
-                        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                            super.onScrollStateChanged(recyclerView, newState);
-                            if(newState == recyclerView.SCROLL_STATE_IDLE){
-                                int position = ((GridLayoutManager) recyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition();
-                                User atUser = parseUsers.get(position);
-                                Log.d(tag, "At user: " + atUser.getFacebookId());
-                                loadFriendEvents(atUser);
-                            }
-                        }
-                    });
                 }
                 else{
                     Toast.makeText(fragAct, "something went wrong fetching users :(", Toast.LENGTH_LONG);
@@ -181,7 +182,7 @@ public class FriendsFragment extends Fragment implements FacebookCallComplete {
         }
     }
 
-    public void updateAdapterDataSet(ArrayList<FacebookQuery.FacebookUser> facebookList) {
+    public void updateFBAdapterDataSet(ArrayList<FacebookQuery.FacebookUser> facebookList) {
         friendsList.clear();
         friendsList.addAll(facebookList);
         friendsAdapter.notifyDataSetChanged();
@@ -205,7 +206,7 @@ public class FriendsFragment extends Fragment implements FacebookCallComplete {
                     for(int i = 0; i < objects.size(); i++){
                         friendEvents.add(objects.get(i));
                     }
-                    friendsAdapter.notifyDataSetChanged();
+                    feedItemAdapter.notifyDataSetChanged();
                 }
                 else {
                     Log.d(tag, "Failed to get events by user :(");
