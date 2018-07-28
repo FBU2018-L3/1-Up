93c7ae7779515929b60754dc70abcf9575704218
LuzCappuccino <luz.camacho323@gmail.com>
2018-07-27 17:26:15 -0700

    Final commit of the day

---
+++ One-Up/app/src/main/java/com/l3/one_up/fragments/PowerUpFragment.java
@@ -0,0 +1,38 @@
+package com.l3.one_up.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.l3.one_up.R;
+
+public class PowerUpFragment extends Fragment {
+
+    public PowerUpFragment() {
+        // Required empty public constructor
+    }
+
+    public static PowerUpFragment newInstance(String param1, String param2) {
+        PowerUpFragment fragment = new PowerUpFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_power_up, container, false);
+    }
+}
