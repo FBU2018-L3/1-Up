6cfeebe044c0fb4d601b74fc741dd32379995d9b
LuzCappuccino <luz.camacho323@gmail.com>
2018-08-06 17:57:46 -0700

    added log statements master don't break on me

--- One-Up/app/src/main/java/com/l3/one_up/adapters/GoalItemAdapter.java
+++ One-Up/app/src/main/java/com/l3/one_up/adapters/GoalItemAdapter.java
@@ -3,6 +3,7 @@ package com.l3.one_up.adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,13 @@ import android.widget.TextView;
 import com.l3.one_up.R;
 import com.l3.one_up.model.Goal;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class GoalItemAdapter extends RecyclerView.Adapter<GoalItemAdapter.ViewHolder> {
+    private static String tag = "GoalItemAdapter";
 
     private ArrayList<Goal> goals;
 
@@ -37,6 +42,26 @@ public class GoalItemAdapter extends RecyclerView.Adapter<GoalItemAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Goal myGoal = goals.get(position);
+        Log.d(tag, "Displaying stats");
+        String goalName = myGoal.getActivity().getName();
+        Log.d(tag, "Current goal for activity: "+ goalName);
+        JSONObject keys = myGoal.getInputType();
+        for(int i = 0; i < keys.names().length(); i++){
+            try {
+                Log.d(tag, "input type: " + keys.names().getString(i) + " value: " + keys.get(keys.names().getString(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        JSONObject progress = myGoal.getProgress();
+        for(int i = 0; i < progress.names().length(); i++){
+            try {
+                Log.d(tag, "Progress input type: " + progress.names().getString(i) + " value: " + progress.get(progress.names().getString(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d(tag, "Is the goal complete: " + myGoal.getIsCompleted());
         holder.tvGoalId.setText(myGoal.getObjectId());
         holder.tvGoalName.setText(myGoal.getActivity().getName());
     }
